670c34c192be95dceaebaa2a6bf73752a4f15aa1
jesseniagonzalezv <38600142+jesseniagonzalezv@users.noreply.github.com>
2021-04-27 15:08:18 +0200

    Add files via upload

--- create_dataset_rttov.sh
+++ create_dataset_rttov.sh
@@ -15,12 +15,19 @@ fout3d_name=`echo $fname3d_in | cut -f 1 -d '.'`
 fout3d_1=`echo $fout3d_name\_1timestep.nc`
 fout3d_rttov=`echo $fout3d_name\_1timestep_rttov.nc`
 
-
 fname2d_in="2d_surface_day_DOM03_ML_20130502T${hr}0000Z.nc"
 fout2d_name=`echo $fname2d_in | cut -f 1 -d '.'`
 fout2d_1=`echo $fout2d_name\_grid.nc`
 fout2d_2=`echo $fout2d_name\_1timestep.nc`
 fout2d_rttov=`echo $fout2d_name\_1timestep_rttov.nc`
+
+gridfile=$path_data_in/hdcp2_de_default_nest_R0156m.nc
+fnameFR_LAND_in=$path_data_in/extpar_hdcp2_de_default_nest_R0156m.nc
+fout_landmask_rttov=$path_data_out/landmask_R0156m_rttov.nc
+
+fname_surface=$path_data_in/GRID_default_3d_fine_DOM03_ML.nc
+fout_surface=$path_data_out/surface_DOM03.nc
+fout_surface_rttov=$path_data_out/surface_DOM03_rttov.nc
 
 : '
 echo 3D generating
@@ -30,22 +37,40 @@ ncwa -a time $path_data_out/$fout3d_1 $path_data_out/$fout3d_rttov
 rm $path_data_out/$fout3d_1
 
 python verify_data.py --path-data-in $path_data_in/$fname3d_in  --path-data-copied $path_data_out/$fout3d_rttov --type-data '3D'
-echo $fout3d_rttov
+echo $fout3d_rttov generated
 '
 
 : '
 echo 2D generating
 
-cdo -P 8 remapnn,myGridDef -setgrid,$path_data_in/hdcp2_de_default_nest_R0156m.nc -selname,v_10m,u_10m,t_s,ps $path_data_in/$fname2d_in $path_data_out/$fout2d_1 
+cdo -P 8 remapnn,myGridDef -setgrid,$gridfile -selname,v_10m,u_10m,t_s,ps $path_data_in/$fname2d_in $path_data_out/$fout2d_1 
 cdo -seltimestep,26 -selvar,t_s,u_10m,v_10m,ps $path_data_out/$fout2d_1  $path_data_out/$fout2d_2 
 ncwa -a height_2,time  $path_data_out/$fout2d_2 $path_data_out/$fout2d_rttov 
 
 python verify_data.py --path-data-in $path_data_in/$fout2d_1  --path-data-copied $path_data_out/$fout2d_rttov --type-data '2D'
 
 rm $path_data_out/$fout2d_1 $path_data_out/$fout2d_2 
-echo $fout2d_rttov
+echo $fout2d_rttov generated
 '
-
-
 done
+
+: '
+echo landmask generating
+
+cdo -P 8 remapnn,myGridDef -setgrid,$gridfile -selname,FR_LAND $fnameFR_LAND_in /$fout_landmask_rttov
+echo $fout_landmask generated
+#'
+
+: '
+echo z_ifc,z_mc,topography_c generating
+
+cdo -P 8 remapnn,myGridDef -setgrid,$gridfile -selname,z_ifc,z_mc,topography_c $fname_surface $fout_surface 
+
+'
+#python create_nc_surface_data_DOM03.py --path-in $fout_surface --path-out $fout_surface_rttov
+echo $fout_surface_rttov generated
+python verify_data.py --path-data-in $fout_surface  --path-data-copied $fout_surface_rttov --type-data 'surface'
+
+#rm $fout_surface
+
 exit 0
--- verify_data.py
+++ verify_data.py
@@ -1,19 +1,16 @@
 import numpy as np
 import xarray as xr
-
 import argparse
 
 def main():
     parser = argparse.ArgumentParser()
     arg = parser.add_argument
-    arg('--path-data-ini', type=str, default='dataset', help='path of the data initial is')
-    arg('--path-data-copied', type=str, default='dataset', help='path of the data copied is' )
-    arg('--type-data', type=str, default='2D', help='2D or 3D' )
+    arg('--path-data-ini', type=str, default='input.nc', help='path of the initial data is')
+    arg('--path-data-copied', type=str, default='input_copied.nc', help='path of the copied data is' )
+    arg('--type-data', type=str, default='2D', help='2D or 3D or surface' )
 
 
     args = parser.parse_args()
-    #path_data_ini='/poorgafile1/climate/hdcp2/2013' #/work/bb1036/b381362/dataset
-    #path_data_copied='/home/jvillarreal'
 
     fname = args.path_data_ini 
     fname_1timestep = args.path_data_copied 
@@ -24,15 +21,16 @@ def main():
 
     print(ds_1timestep.data_vars)
 
-    print('timestep',ds_1timestep.data_vars['time'].values)
     if(args.type_data == '3D'):
       print('Verifing copied data_3D')
+      print('timestep',ds_1timestep.data_vars['time'].values)
       for v in ds_1timestep.data_vars:
         if (v != ('time') and v !=('height_bnds')):         	 
           print(v,':copied correctly',np.array_equal(ds[v].values[5,:,:,:],ds_1timestep[v].values[:,:,:]))
    		
     elif(args.type_data == '2D'):
       print('Verifing copied data_2D')
+      print('timestep',ds_1timestep.data_vars['time'].values)
       for v in ds_1timestep.data_vars:
         if (v != ('ps') and v !=('t_s') and v != ('time') and v !=('height_2')):
           print(v,':copied correctly',np.array_equal(ds[v].values[25,0,:,:],ds_1timestep[v].values[:,:]))
@@ -40,6 +38,18 @@ def main():
         elif(v != ('time') and v !=('height_2')):
           print(v,':copied correctly',np.array_equal(ds[v].values[25,:,:],ds_1timestep[v].values[:,:]))
          	    		    
-            
+    elif(args.type_data == 'surface'):
+      print('Verifing copied data_surface')
+      for v in ds_1timestep.data_vars:
+        if (v == ('z_ifc') and v != ('height_bnds') ):
+          print(v,':copied correctly',np.array_equal(ds[v].values[1:,:,:],ds_1timestep[v].values[:,:,:]))
+        elif (v != ('topography_c') and v != ('height_bnds') ):
+          print(v,':copied correctly',np.array_equal(ds[v].values[:,:,:],ds_1timestep[v].values[:,:,:]))
+                                           
+        elif( v !=('height_bnds')):
+            print(v,':copied correctly',np.array_equal(ds[v].values[:,:],ds_1timestep[v].values[:,:]))
+                        
+
+
 if __name__ == '__main__':
     main()
