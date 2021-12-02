60d406ccf7452f0bdb50bdf040d41996f04ca1cb
jesseniagonzalezv <jessenia.gonzalezv@uni-leipzig.de>
2021-12-02 21:53:23 +0100

    update simulations-code

--- code_test_MODIS/simulation.py
+++ code_test_MODIS/simulation.py
@@ -10,25 +10,46 @@ import pandas as pd
 import netCDF4 #https://www.earthinversion.com/utilities/reading-NetCDF4-data-in-python/
 
 
-def plot_input_ICON(out_file,variable,path_ICON):
+def plot_input_ICON(out_file,variable,path_ICON,dimension_variable,input_data="my_data"):
     '''
     input:
-    variable: name of the variable of 2 dimensions
+    variable: name of the variable of 2 or 3 dimensions
     '''
     ds = xr.open_dataset(path_ICON).compute()
-    #qv_values=ds['qv']      #(level, lat, lon) select top of the atmospheric Nd
-    #reff_values= ds['Reff'] 
-    variable_values = ds[variable].values  #(lat, lon)
-    variable_values_array = variable_values.flatten()  # covert 2d to 1d array 
-    fig,ax = plt.subplots(1,1,figsize = (14,8))
+    
+    nlat = len(ds['lat'])
+    nlon = len(ds['lon'])
+    
+    if(dimension_variable == 3):
+        if(input_data == "example"):
+            n_heigh =len(ds['level']) ### consider also a exception si no es eso q sea level
+            variable_values= ds[variable].transpose('lat', 'lon','level').values
+        else:
+            n_heigh =len(ds['height']) ### consider also a exception si no es eso q sea level
+            variable_values= ds[variable].transpose('lat', 'lon','height').values
+            
+        variable_flated = np.reshape(variable_values, (nlat*nlon,-1))
+   
+    else:
+        #qv_values=ds['qv']      #(level, lat, lon) select top of the atmospheric Nd
+        #reff_values= ds['Reff'] 
+        variable_values = ds[variable].values  #(lat, lon)
+        variable_flated = variable_values.flatten()  # covert 2d to 1d array 
+        
+        fig,ax = plt.subplots(1,1,figsize = (14,8))
 
-    pcm=ax.imshow(np.fliplr(variable_values),origin='lower')#,vmin=100,vmax=1500)
-          #  pcm = axes[i].imshow(np.fliplr(sds_list[x][band]),  origin='lower') #interpolation='nearest'
+        pcm=ax.imshow(np.fliplr(variable_values),origin='lower')#,vmin=100,vmax=1500)
+              #  pcm = axes[i].imshow(np.fliplr(sds_list[x][band]),  origin='lower') #interpolation='nearest'
+        cbar=fig.colorbar(pcm,ax=ax)
+        #plt.show()
+        fig.savefig(out_file+'/'+variable+".png") 
+        plt.close()
 
-    cbar=fig.colorbar(pcm,ax=ax)
-    #plt.show()
-    fig.savefig(out_file+'/'+variable+".png") 
-    plt.close()
+    variable_df = pd.DataFrame(variable_flated)
+    variable_df.describe().to_csv(out_file+ "/"+ input_data+ "_"+variable+"_description.csv")        
+
+
+
 
 def plot_variable_RTTOV(ds_array, variable,out_file,input_data="my_data"):
     fig,axes = plt.subplots(5,8,figsize = (32,20))
@@ -49,8 +70,8 @@ def plot_variable_RTTOV(ds_array, variable,out_file,input_data="my_data"):
                 pcm=ds_array[variable].sel(chan=col).plot(ax=ax, cbar_kwargs={"label":label})
             elif(variable=="Y"):
                 label= '$Radiances/(W\,m^{-2}\,\mu m^{-1}\,sr^{-1})$'
-                pcm=ds_array[variable].sel(chan=col).plot(vmin=0, vmax=1000,ax=ax, cbar_kwargs={"label":label})
-        
+                pcm=ds_array[variable].sel(chan=col).plot(ax=ax, cbar_kwargs={"label":label}) #vmin=0, ,  vmax=10 vmax=600,
+      
             ax.set_title('Channel %d'% (col+1))
     
     #plt.show()
@@ -77,9 +98,14 @@ def output_RTTOV(out_file,variable,path_OUTPUT_RTTOV,input_data="my_data"):
     #BRDF_flated = .reshape(-1,)
     nlat = len(ds['lat'])
     nlon = len(ds['lon'])
+    n_bands =len(ds['chan'])
+    print("number of bands",n_bands)
+
     
-    pd.set_option('display.float_format', lambda x: '%.4f' % x)
-    pd.set_option('display.max_columns', None)
+#     pd.set_option('display.float_format', lambda x: '%.4f' % x)
+#     pd.set_option('display.max_columns', None)
+
+
 
     # convert to Dataframe 
     variable_values= ds[variable].transpose('lat', 'lon','chan').values
@@ -87,11 +113,19 @@ def output_RTTOV(out_file,variable,path_OUTPUT_RTTOV,input_data="my_data"):
     variable_df = pd.DataFrame(variable_flated)
     #variable_df.describe()
 
+    for i in range((n_bands)-1):
+        count_nan = variable_df[i].isnull().sum()
+        print (i,'Count of NaN: ' + str(count_nan))    
+    pd.set_option('display.float_format', lambda x: '%.3f' % x)
+    
+
+    variable_df.describe().to_csv(out_file+ "/"+ input_data+ "_"+variable+"_description.csv")       
+    
     print("ok dataframe")
     #plot figure 
     plot_variable_RTTOV(ds_array=ds, variable=variable,out_file=out_file,input_data=input_data)
         
-        
+     
 
 
 
@@ -103,20 +137,40 @@ def main():
     arg('--path-OUTPUT-RTTOV', type=str, default='/home/jvillarreal/Documents/phd/github/output-rttov/output-test-2-modis.nc', help='path of the dataset the output of RTTOV')
 
     arg('--path-output', type=str, default='/home/jvillarreal/Documents/phd/output', help='path of the output data is')
+    arg('--input-data', type=str, default='test-2', help='name of the input to plot test-2')
+    arg('--variable-input', type=str, default='qnc', help='name of the input thermo variable ')
+    arg('--dimension-variable', type=int, default=3, help='dimension of the input variable ')
+    arg('--type-simulation', type=str, default='ICON', help='radiances or ICON')
+
 
     args = parser.parse_args()
 
     path_ICON = args.path_ICON
+    input_data = args.input_data
     path_OUTPUT_RTTOV = args.path_OUTPUT_RTTOV 
     path_output=args.path_output
+    variable_input=args.variable_input
+    dimension_variable=args.dimension_variable
+    type_simulation=args.type_simulation
     
     #plot_input_ICON(out_file=path_output,variable="lwp",path_ICON=path_ICON)
 #     output_RTTOV(out_file=path_output,variable='brdf',path_OUTPUT_RTTOV=path_OUTPUT_RTTOV)
 #     output_RTTOV(out_file=path_output,variable='Y',path_OUTPUT_RTTOV=path_OUTPUT_RTTOV)
 
-    output_RTTOV(out_file=path_output,variable='brdf',path_OUTPUT_RTTOV=path_OUTPUT_RTTOV,input_data="ex_data")
-    output_RTTOV(out_file=path_output,variable='Y',path_OUTPUT_RTTOV=path_OUTPUT_RTTOV,input_data="ex_data")
+#     output_RTTOV(out_file=path_output,variable='brdf',path_OUTPUT_RTTOV=path_OUTPUT_RTTOV,input_data=input_data)
+#     output_RTTOV(out_file=path_output,variable='Y',path_OUTPUT_RTTOV=path_OUTPUT_RTTOV,input_data=input_data)
     
+    if(type_simulation=='ICON'):
+
+        plot_input_ICON(out_file=path_output,variable=variable_input, path_ICON=path_ICON, dimension_variable=dimension_variable, input_data=input_data)
+        
+    else:
+        output_RTTOV(out_file=path_output,variable='Y',path_OUTPUT_RTTOV=path_OUTPUT_RTTOV,input_data=input_data)
+        #output_RTTOV(out_file=path_output,variable='brdf',path_OUTPUT_RTTOV=path_OUTPUT_RTTOV,input_data=input_data)
+
+
+        
+
     
     
 #     sys.stdout = open(out_file+'/log_PCA_radiances.txt','wt')
---
+++ code_test_MODIS/simulations.sh
@@ -0,0 +1,53 @@
+#!/bin/sh
+
+
+
+# '''
+# Read simulations input or output and plot it
+
+# '''
+
+
+
+# ''' ya probe
+# ## output of the rttov with the test-2 
+# # python simulation.py --path-OUTPUT-RTTOV '/home/jvillarreal/Documents/phd/github/output-rttov/output-test-2.nc' --path-output '/home/jvillarreal/Documents/phd/output' --input-data 'output-test-2-old'
+
+# ### output 3 channels Odran example with the test-2  channel 2, 7, 32-----------,dom1 dom1, dom_nstreams 8
+# python simulation.py --path-OUTPUT-RTTOV '/home/jvillarreal/Documents/phd/github/output-rttov/outputs_test_3chan_v1.nc' --path-output '/home/jvillarreal/Documents/phd/output' --input-data 'outputs_test_3chan_v1'
+
+# ## output of the rttov version anterior (more cleaning up) with the test-2    channel 7 and 32-----------,dom1 dom1, dom_nstreams 4
+# python simulation.py --path-OUTPUT-RTTOV '/home/jvillarreal/Documents/phd/github/output-rttov/rttov-msg-7-32.nc' --path-output '/home/jvillarreal/Documents/phd/output' --input-data 'rttov-msg-7-32'
+
+
+# ## output of the rttov version anterior (more cleaning up) with the test-2  channel 32 -----------,dom1 dom1, dom_nstreams 4
+# python simulation.py --path-OUTPUT-RTTOV '/home/jvillarreal/Documents/phd/github/output-rttov/rttov-msg-32.nc' --path-output '/home/jvillarreal/Documents/phd/output' --input-data 'rttov-msg-32'
+
+# '''
+
+# -----------,dom1 dom1, dom_nstreams 8
+# python simulation.py --path-OUTPUT-RTTOV '/home/jvillarreal/Documents/phd/github/output-rttov/rttov-13-msg-2-7-32.nc' --path-output '/home/jvillarreal/Documents/phd/output' --input-data 'rttov-13-msg-2-7-32'
+
+
+#-----------,dom1 dom1, dom_nstreams 8
+# python simulation.py --path-OUTPUT-RTTOV '/home/jvillarreal/Documents/phd/github/output-rttov/rttov-12-msg-1-36.nc' --path-output '/home/jvillarreal/Documents/phd/output' --input-data 'rttov-12-msg-1-36' --type-simulation 'radiances'
+
+# python simulation.py --path-OUTPUT-RTTOV '/home/jvillarreal/Documents/phd/github/output-rttov/rttov-msg-7-32.nc' --path-output '/home/jvillarreal/Documents/phd/output' --input-data 'rttov-msg-7-32' --type-simulation 'radiances'
+
+
+# python simulation.py --path-OUTPUT-RTTOV '/home/jvillarreal/Documents/phd/github/output-rttov/rttov-13-msg.nc' --path-output '/home/jvillarreal/Documents/phd/output' --input-data 'rttov-13-msg-without-ls' --type-simulation 'radiances'
+
+python simulation.py --path-OUTPUT-RTTOV '/home/jvillarreal/Documents/phd/github/output-rttov/rttov-13-msg-converted.nc' --path-output '/home/jvillarreal/Documents/phd/output' --input-data 'rttov-13-converted-rad-y' --type-simulation 'radiances'
+
+python simulation.py --path-OUTPUT-RTTOV '/home/jvillarreal/Documents/phd/github/output-rttov/rttov-13-msg-converter-32-33-34.nc' --path-output '/home/jvillarreal/Documents/phd/output' --input-data 'rttov-13-converted-rad-32-33-34-y' --type-simulation 'radiances'
+
+python simulation.py --path-OUTPUT-RTTOV '/home/jvillarreal/Documents/phd/github/output-rttov/rttov-13-msg-converter-29-30-32-33-34.nc' --path-output '/home/jvillarreal/Documents/phd/output' --input-data 'rttov-13-converted-rad-29-30-32-33-34-y' --type-simulation 'radiances'
+
+python simulation.py --path-OUTPUT-RTTOV '/home/jvillarreal/Documents/phd/github/output-rttov/rttov-13-msg-converter-28-35-36.nc' --path-output '/home/jvillarreal/Documents/phd/output' --input-data 'rttov-13-converted-rad-28-35-36' --type-simulation 'radiances'
+
+# '''
+# python simulation.py --path-ICON '/home/jvillarreal/Documents/phd/dataset/subset_rttov_T12_old.nc' --path-output '/home/jvillarreal/Documents/phd/output' --input-data 'subset-data--old' --variable-input 'clc' --dimension-variable 3 
+
+
+# python simulation.py --path-ICON '/home/jvillarreal/Documents/phd/dataset/subset_rttov_T12.nc' --path-output '/home/jvillarreal/Documents/phd/output' --input-data 'subset-data-vf' --variable-input 'clc' --dimension-variable 3 
+# '''
